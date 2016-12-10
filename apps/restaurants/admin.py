30f12996a22c26bcaf8b5b1dd03a9b9d6dd6960a
Darwin Quiroz <darwin_quiroz04@hotmail.com>
2016-12-10 13:27:55 -0500

    Pagina de inicio y logueo con facebook

--- apps/restaurants/admin.py
+++ apps/restaurants/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Category, City, Tip, Restaurant, Payment, Stablishment
 
-# Register your models here.
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+	list_display = ('name',)
+	list_filter = ('name',)
+
+@admin.register(City)
+class AdminCity(admin.ModelAdmin):
+	list_display = ('name',)
+	list_filter = ('name',)
+
+@admin.register(Tip)
+class AdminTip(admin.ModelAdmin):
+	list_display = ('restaurant', 'user', 'conten',)
+	list_filter = ('restaurant', 'user',)
+
+@admin.register(Restaurant)
+class AdminRestaurant(admin.ModelAdmin):
+	list_display = ('name', 'description',)
+	list_filter = ('payment', 'category', 'name',)
+
+admin.site.register(Payment)
+class AdminPayment(admin.ModelAdmin):
+	list_display = ('pay',)
+	list_filter = ('pay',)
+
+admin.site.register(Stablishment)
+class AdminStablishment(admin.ModelAdmin):
+	list_display = ('restaurant', 'city', 'addres',)
+	list_filter = ('restaurant', 'city',)
